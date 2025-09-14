fd19c2fed13818a0ad1dc3db80178d1bb9ac31c9
Rachel Sherard <rsherard@mac.com>
2025-09-14 14:50:58 -0400

    test counter

--- functions/counter.js
+++ functions/counter.js
@@ -3,7 +3,8 @@ import { getStore } from "@netlify/blobs";
 export default async (request, context) => {
 
   const store = getStore("subcount");
-  store.set("count", 7);
+  // store.set("count", 7);
+  console.log(store);
   const count = await store.get("count");
 
   return new Response("The Sub Count is: " + count, {
