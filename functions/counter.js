42cdabcf580328f6d715cba8ff8d7eb8e32b3cf2
Rachel Sherard <rsherard@mac.com>
2025-09-14 14:19:49 -0400

    attempt to add subcounter

---
+++ functions/counter.js
@@ -0,0 +1,12 @@
+import { getStore } from "@netlify/blobs";
+
+export default async (request, context) => {
+
+  const store = getStore("subcount");
+  store.set("count", 7);
+  const count = await store.get("count");
+
+  return new Response("The Sub Count is: " + count, {
+    headers: { "content-type": "text/plain" },
+  });
+};
